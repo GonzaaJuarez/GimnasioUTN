5d7430d2289288f5caf896f8a6fd7958d68ed646
GonzaJuarez <gonzaloarieljuarez11@gmail.com>
2026-05-31 21:37:27 -0300

    Crear estructura base de plantillas

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
+@app.route("/horarios")
+def horarios():
+    return render_template("horarios.html")
+
+@app.route("/profesores")
+def profesores():
+    return render_template("profesores.html")
+
+@app.route("/precios")
+def precios():
+    return render_template("precios.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
